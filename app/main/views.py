d083ebb6b50bb3a010ece7a3437c06118854ff6b
Alexei Bastidas <bastidas.alexei@gmail.com>
2015-11-19 13:19:48 -0800

    119pm
    austin hooked it up with the jqueries

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,5 @@
 from flask import render_template, redirect, url_for, abort, flash, request,\
-    current_app, make_response, g
+    current_app, make_response, g, jsonify
 from flask.ext.login import login_required, current_user
 from flask.ext.sqlalchemy import get_debug_queries
 from . import main
