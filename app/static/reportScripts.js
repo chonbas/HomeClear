251e9c76aa764ea89fdf7cbf0567391cf1316db2
Alexei Bastidas <bastidas.alexei@gmail.com>
2015-11-24 16:59:29 -0800

    Added Listings View
    added listings view, still need listings to be passed in

--- app/static/reportScripts.js
+++ app/static/reportScripts.js
@@ -1,44 +1,44 @@
 $(document).ready(function(){
   $(".reportTabs").hide();
   $("#basicInfo").show();
+});
 
-  $("#schoolInfoBtn").click(function(){
-    $("#basicInfo").hide();
-    $("#crimeInfo").hide();
-    $("#geoInfo").hide();
-    $("#miscInfo").hide();
-    $("#schoolInfo").show();
-  });
+$("#schoolInfoBtn a").click(function(){
+  $("#basicInfo").hide();
+  $("#crimeInfo").hide();
+  $("#geoInfo").hide();
+  $("#miscInfo").hide();
+  $("#schoolInfo").show();
+});
 
-  $("#basicInfoBtn").click(function(){
-    $("#basicInfo").show();
-    $("#crimeInfo").hide();
-    $("#geoInfo").hide();
-    $("#miscInfo").hide();
-    $("#schoolInfo").hide();
-  });
+$("#basicInfoBtn a").click(function(){
+  $("#basicInfo").show();
+  $("#crimeInfo").hide();
+  $("#geoInfo").hide();
+  $("#miscInfo").hide();
+  $("#schoolInfo").hide();
+});
 
-  $("#crimeInfoBtn").click(function(){
-    $("#basicInfo").hide();
-    $("#crimeInfo").show();
-    $("#geoInfo").hide();
-    $("#miscInfo").hide();
-    $("#schoolInfo").hide();
-  });
+$("#crimeInfoBtn a").click(function(){
+  $("#basicInfo").hide();
+  $("#crimeInfo").show();
+  $("#geoInfo").hide();
+  $("#miscInfo").hide();
+  $("#schoolInfo").hide();
+});
 
-  $("#geoInfoBtn").click(function(){
-    $("#basicInfo").hide();
-    $("#crimeInfo").hide();
-    $("#geoInfo").show();
-    $("#miscInfo").hide();
-    $("#schoolInfo").hide();
-  });
+$("#geoInfoBtn a").click(function(){
+  $("#basicInfo").hide();
+  $("#crimeInfo").hide();
+  $("#geoInfo").show();
+  $("#miscInfo").hide();
+  $("#schoolInfo").hide();
+});
 
-  $("#miscInfoBtn").click(function(){
-    $("#basicInfo").hide();
-    $("#crimeInfo").hide();
-    $("#geoInfo").hide();
-    $("#miscInfo").show();
-    $("#schoolInfo").hide();
-  });
+$("#miscInfoBtn a").click(function(){
+  $("#basicInfo").hide();
+  $("#crimeInfo").hide();
+  $("#geoInfo").hide();
+  $("#miscInfo").show();
+  $("#schoolInfo").hide();
 });
